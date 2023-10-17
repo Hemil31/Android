ffcde1e466e9f365bcbf7390ffd768ca3cd8a61b
Student name <HemilDudhat2001@gmail.com>
2023-10-17 11:20:40 +0530

    "update"

---
+++ app/src/main/java/com/example/apidemo/MyAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.apidemo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(var con : Context,var userlist : List<MyDataItems>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = LayoutInflater.from(con).inflate(R.layout.raw_list_data,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return userlist.count()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = userlist[position].name
+        holder.subTitle.text = userlist[position].subTitle
+
+        holder.image.setImageResource(userlist[position].image.toInt())
+
+    }
+
+
+    class ViewHolder(v: View):RecyclerView.ViewHolder(v){
+        var image = v.findViewById<ImageView>(R.id.imgLogo)
+        var title = v.findViewById<TextView>(R.id.txtTitle)
+        var subTitle = v.findViewById<TextView>(R.id.txtSubTitle)
+    }
+}
---
+++ app/src/main/java/com/example/apidemo/MyDataItems.kt
@@ -0,0 +1,4 @@
+package com.example.apidemo
+
+data class MyDataItems(val image: String
+, val name: String,val subTitle: String)
